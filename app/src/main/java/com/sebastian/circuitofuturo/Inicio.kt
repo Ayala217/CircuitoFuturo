c7ed12d3369556e80dc7ac686b4b3aea24006af6
Andrea Urdaneta <andreaurro@unisabana.edu.co>
2024-11-06 22:28:55 -0500

    Avances Menu

--- app/src/main/java/com/sebastian/circuitofuturo/Inicio.kt
+++ app/src/main/java/com/sebastian/circuitofuturo/Inicio.kt
@@ -1,6 +1,11 @@
 package com.sebastian.circuitofuturo
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +15,81 @@ class Inicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_inicio)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val tvMenu: TextView = findViewById(R.id.tvMenu)
+
+        tvMenu.setOnClickListener { view ->
+            showPopupMenu(view)
         }
     }
+
+    private fun showPopupMenu(view: View) {
+        val popupMenu = PopupMenu(this, view)
+        popupMenu.inflate(R.menu.menu) // Usa el archivo XML de menú creado
+
+        // Maneja los clics en las opciones del menú
+        popupMenu.setOnMenuItemClickListener { menuItem: MenuItem ->
+            when (menuItem.itemId) {
+                R.id.IniciarSesion -> {
+
+                    val intent = Intent(this@Inicio, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                R.id.Principal -> {
+
+                    val intent = Intent(this@Inicio, Inicio::class.java)
+                    startActivity(intent)
+                    true
+
+                }
+                R.id.Inscripciones -> {
+                    val intent = Intent(this@Inicio, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.Programacion -> {
+                    val intent = Intent(this@Inicio, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.Ranking -> {
+                    val intent = Intent(this@Inicio, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.Partidos -> {
+                    val intent = Intent(this@Inicio, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.Torneos -> {
+                    val intent = Intent(this@Inicio, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.Galeria -> {
+                    val intent = Intent(this@Inicio, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+        popupMenu.show() // Muestra el menú emergente
+    }
 }
+
+
+
+
+
+
+
